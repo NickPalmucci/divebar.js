4e2f18c0eac21b4d697199fce5f7808d6d77b1d6
Mucciano <nicolas.eli.palmucci@gmail.com>
2019-08-07 16:34:09 -0400

    material ui TextField with emoji toggle InputAdornment

---
+++ src/components/stickers/sticker-text.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InsertEmoticon from '@material-ui/icons/InsertEmoticon';
+
+export default (props) => {
+    const inputStyle = {
+        display: "block",
+        width: '100%'
+    };
+
+    const { setAppState, stickerText, showEmoji } = props;
+
+    const handleShowEmoji = () => {
+        setAppState({showEmoji: !showEmoji})
+    };
+
+    return (
+        <TextField
+            style={inputStyle}
+            value={stickerText}
+            fullWidth
+            multiline={true}
+            onChange={event => setAppState({stickerText: event.target.value})}
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <InsertEmoticon onClick={handleShowEmoji}/>
+                    </InputAdornment>
+                )
+            }}
+        />
+    )
+}
+
