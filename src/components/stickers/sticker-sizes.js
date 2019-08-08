d0d2bd34d8fd3a502924f3c2f392e587c7fada28
Mucciano <nicolas.eli.palmucci@gmail.com>
2019-08-08 13:19:43 -0400

    do size to dimension mapping in component and update state with new values

--- src/components/stickers/sticker-sizes.js
+++ src/components/stickers/sticker-sizes.js
@@ -4,20 +4,28 @@ import Button from '@material-ui/core/Button';
 
 const styles = {
     button: {
-        margin: '1rem 0'
+        margin: '0.5rem 0'
     }
 };
 
 const buttonNames = ['small', 'medium', 'large', 'xxxl'];
 
+const sizeMap = {
+    'small': {stickerHeight: 100, stickerWidth: 100},
+    'medium': {stickerHeight: 200, stickerWidth: 200},
+    'large': {stickerHeight: 300, stickerWidth: 250},
+    'xxxl': {stickerHeight: 400, stickerWidth: 350}
+};
+
 const sizePick = (props) => {
     const {classes, stickerSize, setAppState} = props;
 
     const handleClick = (size) => {
-        setAppState({stickerSize: size})
+        const dimensions = sizeMap[size];
+        setAppState({...dimensions, stickerSize: size})
     };
 
-    const buttons = buttonNames.map((name)=>{
+    const buttons = buttonNames.map((name, i)=>{
         return (
             <Button
                 color="primary"
@@ -25,6 +33,7 @@ const sizePick = (props) => {
                 disabled={stickerSize === name}
                 className={classes.button}
                 onClick={event=>handleClick(name)}
+                key={i}
             >
                 {name}
             </Button>
