62d2f3c989dde237b42fdb845e174e4d8c10ae9e
Mucciano <nicolas.eli.palmucci@gmail.com>
2019-10-08 16:01:48 -0400

    add new menuStyle with position absolute and new Fab for brushes modal

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import StickerIcon from '../stickers/sticker-svgrepo-com';
+import {FormatPaint} from '@material-ui/icons';
+
+const menuStyle= {
+    padding: '0 1rem 1rem 1rem',
+    position: 'absolute',
+    top: '1rem',
+    left: '1rem',
+    zIndex: 9000,
+    display: 'flex',
+    flexDirection: 'column'
+};
+
+const buttonStlye = {
+  marginTop: '0.5rem'
+};
 
 export default (props) => {
 
     return (
-        <div style={{float: 'right', padding: '0 1rem 1rem 1rem'}}>
-            <Fab onClick={event => {props.setAppState({modal: true})}}>
+        <div style={menuStyle}>
+            <Fab
+                style={buttonStlye}
+                onClick={event => {props.setAppState({modal: true, modalType: 'brush'})}}
+            >
+                <FormatPaint/>
+            </Fab>
+            <Fab
+                style={buttonStlye}
+                onClick={event => {props.setAppState({modal: true, modalType: 'sticker'})}}
+            >
                 <StickerIcon/>
             </Fab>
         </div>
